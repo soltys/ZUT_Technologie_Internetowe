c7d69acb6fd736617c449b7cf54cb0488d60820b
Paweł Sołtysiak <soltys@uznam.org>
2013-11-13 17:54:59 +0100

    Added routing tests

---
+++ code/PSBlog.Test/RoutingTests.cs
@@ -0,0 +1,56 @@
+﻿using Messum.UI.Test.Helpers;
+using NUnit.Framework;
+
+namespace PSBlog.Test
+{
+    [TestFixture]
+    class RoutingTests
+    {
+        //Slash
+        [Test]
+        public void When_Url_Slash_Home_Index()
+        {
+            const string url = "~/";
+            RouteHelpers.TestRoute(url, new
+            {
+                Controller = "Home",
+                Action = "Index",
+            });
+        }
+        //User
+        [Test]
+        public void slash_user_user_index()
+        {
+            const string url = "~/User";
+            RouteHelpers.TestRoute(url, new
+            {
+                Controller = "User",
+                Action = "Index",
+            });
+        }
+
+        [Test]
+        public void slash_user_register_user_register()
+        {
+            const string url = "~/User/Register";
+            RouteHelpers.TestRoute(url, new
+            {
+                Controller = "User",
+                Action = "Register",
+            });
+        }
+
+        [Test]
+        public void slash_user_login_user_login()
+        {
+            const string url = "~/User/Login";
+            RouteHelpers.TestRoute(url, new
+            {
+                Controller = "User",
+                Action = "Login",
+            });
+        }
+
+       
+    }
+}
