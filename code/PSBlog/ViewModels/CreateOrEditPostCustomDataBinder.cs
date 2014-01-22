abe0a48fb6d15095be404ac979e3e36b3f76e615
Paweł Sołtysiak <soltys@uznam.org>
2014-01-22 21:36:32 +0100

    finish

---
+++ code/PSBlog/ViewModels/CreateOrEditPostCustomDataBinder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PSBlog.ViewModels
+{
+    public class CreateOrEditPostCustomDataBinder : DefaultModelBinder
+    {
+        public override object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            if (bindingContext.ModelType == typeof(CreateOrEditPostModel))
+            {
+                HttpRequestBase request = controllerContext.HttpContext.Request;
+
+                int blogId = int.Parse(request.Form.Get("Blog.Id"));
+                string title = request.Form.Get("Post.Title");
+                int postId = int.Parse(request.Form.Get("Post.Id") ?? "0");
+                string content = request.Form.Get("Post.Content");
+
+                return new CreateOrEditPostModel
+                {
+                    Blog = new Models.Blog { Id = blogId },
+                    Post = new Models.Post { Id = postId, Title = title, Content = content }
+                };
+            }
+            else
+            {
+                return base.BindModel(controllerContext, bindingContext);
+            }
+        }
+    }
+}
---
+++ code/PSBlog/ViewModels/CreateOrEditPostModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PSBlog.Models;
+namespace PSBlog.ViewModels
+{
+    public class CreateOrEditPostModel
+    {
+        public Blog Blog { get; set; }
+        public Post Post { get; set; }
+    }
+}
